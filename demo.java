1f78f9b4c58cf22c0fb223bfd1c651c49742dfd1
Akshay Singare <singareakshay937@gmail.com>
2025-08-06 09:23:18 +0530

    Find Prime Numer Inside the String

--- demo.java
+++ demo.java
@@ -1,38 +1,38 @@
 package leetcode;
 
-class Solution {
-	public int[] plusOne(int[] digits) {
+public class demo {
 
-		for (int i = digits.length - 1; i >= 0; i--) {
-			if (i == 0 && digits[i] == 9) {
-				int dig[] = new int[digits.length + 1];
-				dig[0] = 1;
-				digits[i] = 0;
-				for (int j = 0, k = 1; j < digits.length; j++, k++) {
-					dig[k] = digits[j];
-				}
-				return dig;
-			} else if (digits[i] == 9)
-				digits[i] = 0;
-			else {
-				digits[i]++;
-				break;
-			}
-
-		}
-		return digits;
-	}
-}
-
-class demo {
 	public static void main(String x[]) {
+		StringBuilder sb = new StringBuilder();
 
-		Solution s = new Solution();
+		String str = "ka29p3d7e45";
+		boolean flag = false;
+		for (char i = 0; i < str.length() - 1; i++) {
+			char ch = str.charAt(i);
 
-		int a[] = { 7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1,
-				1, 7, 4, 0, 0 };
-		for (long num : s.plusOne(a)) {
-			System.out.print(num);
+			if ((int) ch >= 48 && 57 >= (int) ch) {
+				sb.append(ch);
+				flag = true;
+			} else if (flag) {
+				sb.append(" ");
+				flag = false;
+			}
+		}
+		System.out.println(sb + "\n");
+		String st = sb.toString();
+		String arr[] = st.split(" ");
+
+		for (int i = 0; i < arr.length; i++) {
+			int a = Integer.parseInt(arr[i]);
+			boolean check = true;
+			for (int j = 2; j <= a / 2; j++) {
+				if (a % j == 0) {
+					check = false;
+					break;
+				}
+			}
+			if (check)
+				System.out.println(a);
 		}
 
 	}
