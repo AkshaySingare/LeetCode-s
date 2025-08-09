dceb1b9f7a6f66318ea03026b198e8ab046a5d19
Akshay Singare <singareakshay937@gmail.com>
2025-08-09 11:54:05 +0530

    String to Integer

---
+++ StringtoInteger_08.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+class Solution {
+	public int myAtoi(String s) {
+		StringBuilder sb = new StringBuilder();
+		s = s.trim();
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if ((i == 0 && (ch == '-' || ch == '+')) || (ch >= '0' && ch <= '9')) {
+				sb.append(ch);
+			} else if (i >= 0 && !(ch >= '0' && ch <= '9')) {
+				break;
+			}
+		}
+		if (sb.length() == 1 && !(s.charAt(0) >= '0' && s.charAt(0) <= '9')) {
+			sb.setCharAt(0, '0');
+		} else if (sb.length() == 0)
+			return 0;
+
+		try {
+			Integer.parseInt(sb.toString());
+		} catch (Exception e) {
+			if (sb.charAt(0) == '-') {
+				return Integer.MIN_VALUE;
+			} else {
+				return Integer.MAX_VALUE;
+			}
+			// TODO: handle exception
+		}
+		return Integer.parseInt(sb.toString());
+	}
+}
+
+public class StringtoInteger_08 {
+	public static void main(String args[]) {
+		String str = " b11228552307";
+		Solution s = new Solution();
+
+		System.out.println(s.myAtoi(str));
+	}
+}
