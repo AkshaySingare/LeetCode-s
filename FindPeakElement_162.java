996a7cf70ce916be1308fc63ef741e3ee30adc6f
Akshay Singare <158290336+AkshaySingare@users.noreply.github.com>
2026-02-09 15:28:35 +0530

    used Binary Search
    A peak element is an element that is strictly greater than its neighbors.
    
    Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
    
    You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
    
    You must write an algorithm that runs in O(log n) time.

---
+++ FindPeakElement_162.java
@@ -0,0 +1,47 @@
+package leetCodes;
+
+public class FindPeakElement_162 {
+
+	  public static int findPeakElement(int[] nums) {
+	        int Iind = 0;
+	        int Jind = 0;
+	        int max = Integer.MIN_VALUE;
+	        for(int i=0,j=nums.length/2;i<=nums.length/2;i++,j++){
+
+	            try{
+	            if(nums[i]>nums[i+1]){                
+	                if(max<nums[i]){
+	                     max = nums[i];
+	                     Iind  = i;
+	                }
+	                   
+	            }else {                
+	                    if(max < nums[i+1]){
+	                        max = nums[i+1];
+	                        Iind = i+1;
+	                    }
+	            }
+	            if(j<nums.length-1 && nums[j]>nums[j+1]){
+	                 if(max < nums[j]){
+	                    Jind = j;
+	                    max = nums[j];
+	                 }
+	            }else{
+	                if(max < nums[j+1]){
+	                    Jind = j+1;
+	                    max = nums[j+1];
+	                 }
+	            }
+	            }catch(ArrayIndexOutOfBoundsException e){
+
+	            }
+	            System.out.println(Iind);
+	        }
+	        return nums[Iind]>nums[Jind]?Iind:Jind;
+
+	    }
+	public static void main(String[] args) {
+		System.out.println(findPeakElement(new int[] {1,2}));
+	}
+
+}
