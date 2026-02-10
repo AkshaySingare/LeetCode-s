b59489e51ac97e8ad42491ebfd274b0d4ffb5af3
Akshay Singare <158290336+AkshaySingare@users.noreply.github.com>
2026-02-10 10:08:24 +0530

    Used Binary Search To find Target Element
    There is an integer array nums sorted in ascending order (with distinct values).
    
    Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
    
    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ SearchInRotatedSortedArray_33.java
@@ -0,0 +1,39 @@
+package leetCodes;
+
+public class SearchInRotatedSortedArray_33 {
+	
+	  public static int search(int[] nums, int target) {
+
+	        
+	        int start = 0;
+	        int end = nums.length - 1;
+
+	        while(start <= end){
+
+	            int mid = start + (end - start)/2;
+
+	            if(nums[mid]==target) return mid;
+	            if(nums[start] <= nums[mid]){ // left sorted
+	                if(nums[start] <= target && nums[mid] >= target){
+	                    end = mid-1;
+	                }else{
+	                    start = mid+1;
+	                }
+	            }else{ // right sorted
+	                if(nums[mid] <= target && nums[end] >= target){
+	                   start = mid+1;
+	                }else{
+	                    end = mid-1;
+	                }
+	            }
+
+	        }
+	    return -1;
+	    }
+	public static void main(String[] args) {
+
+		System.out.println(search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 5));
+
+	}
+
+}
